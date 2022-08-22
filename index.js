a54a1e24dc9556a47478a3c46e8f98d4bdd6662b
fridah karimi <fridah.karimi@student.moringaschool.com>
2022-08-22 11:35:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name:"Sam",
+    streetAddress:"11 Broadway"
+}
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...employee,
+        streetAddress : "11 Broadway"
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name){
+    let newEmployee = {
+        ...employee
+    }
+    delete newEmployee.name;
+    return newEmployee;
+}
+function  destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name;
+    return employee;
+}
